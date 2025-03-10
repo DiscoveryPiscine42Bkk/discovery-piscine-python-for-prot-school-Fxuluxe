be20bae42c2ef9a50c6c6d7bb3f9e84f8c19f559
Fxuluxe <40676@prot.ac.th>
2025-03-10 14:23:02 +0700

    Delete cell02 directory

--- cell02/ex02/password.py
+++
@@ -1,8 +0,0 @@
-correct_password = "Python is awesome"
-
-user_input = input("Enter password: ")
-
-if user_input == correct_password:
-    print("ACCESS GRANTED")
-else:
-    print("ACCESS DENIED")
