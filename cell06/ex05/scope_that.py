514e13467f5551d44b75e6747a7eeb35c15c74f1
Fxuluxe <40676@prot.ac.th>
2025-03-10 22:48:43 +0700

    Add files via upload

---
+++ cell06/ex05/scope_that.py
@@ -0,0 +1,8 @@
+def add_one(number):
+    return number + 1
+
+my_var = 5
+print(f"Initial value of my_var: {my_var}")
+
+my_var = add_one(my_var)
+print(f"Value of my_var after calling add_one: {my_var}")
