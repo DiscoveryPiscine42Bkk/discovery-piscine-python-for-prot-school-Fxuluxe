8db9a9e8127fabb88c5b605777ed56e81839ff09
Fxuluxe <40676@prot.ac.th>
2025-03-10 22:45:35 +0700

    Add files via upload

---
+++ cell06/ex02/downcase_all.py
@@ -0,0 +1,10 @@
+import sys
+
+def downcase_it(input_string):
+    return input_string.lower()
+
+if len(sys.argv) == 1:
+    print("none")
+else:
+    for arg in sys.argv[1:]:
+        print(downcase_it(arg))
