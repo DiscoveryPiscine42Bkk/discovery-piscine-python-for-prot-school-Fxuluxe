ba1ff737ea98a386338ce690fde8a8a2147078e3
Fxuluxe <40676@prot.ac.th>
2025-03-10 22:43:55 +0700

    Add files via upload

---
+++ cell05/ex14/free_range.py
@@ -0,0 +1,18 @@
+import sys
+
+def free_range():
+    if len(sys.argv) != 3:
+        print("none")
+        return
+   
+    try:
+        start = int(sys.argv[1])
+        end = int(sys.argv[2])
+    except ValueError:
+        print("none")
+        return
+ 
+    print(list(range(start, end)))
+
+if __name__ == "__main__":
+    free_range()
