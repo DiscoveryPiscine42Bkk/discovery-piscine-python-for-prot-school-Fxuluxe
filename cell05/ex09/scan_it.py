40ea22dfdf1e39660af8c94112a8c017263b0555
Fxuluxe <40676@prot.ac.th>
2025-03-11 11:36:03 +0700

    Update scan_it.py

--- cell05/ex09/scan_it.py
+++ cell05/ex09/scan_it.py
@@ -1,1 +1,11 @@
-
+import sys
+if len(sys.argv) != 3:
+    print("none")  
+else:
+    keyword = sys.argv[1]
+    string = sys.argv[2]
+    count = string.count(keyword)
+    if count == 0:
+        print("none")
+    else:
+        print(count)
