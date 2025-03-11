90b3f86d48ce8dbdd4e0a6d054e01f1a207fbbd7
Fxuluxe <40676@prot.ac.th>
2025-03-11 11:31:11 +0700

    Update aff_rev_params.py

--- cell05/ex08/aff_rev_params.py
+++ cell05/ex08/aff_rev_params.py
@@ -1,12 +1,1 @@
-import sys
 
-def aff_rev_params():
-    if len(sys.argv) < 2:
-        print("none")
-    else:
-        reversed_params = sys.argv[1:][::-1]
-        for param in reversed_params:
-            print(param)
-
-if __name__ == "__main__":
-    aff_rev_params()
