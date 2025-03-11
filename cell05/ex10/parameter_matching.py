4b99dbf276423b0a265eaaddf0411b5d652d4933
Fxuluxe <40676@prot.ac.th>
2025-03-11 11:31:33 +0700

    Update parameter_matching.py

--- cell05/ex10/parameter_matching.py
+++ cell05/ex10/parameter_matching.py
@@ -1,18 +1,1 @@
-import sys
 
-def parameter_matching():
-    if len(sys.argv) != 2:
-        print("none")
-        return
-  
-    expected_word = sys.argv[1]
-   
-    user_input = input("Enter a word: ")
-   
-    if user_input == expected_word:
-        print("Good job!")
-    else:
-        print("Nope, sorry...")
-
-if __name__ == "__main__":
-    parameter_matching()
