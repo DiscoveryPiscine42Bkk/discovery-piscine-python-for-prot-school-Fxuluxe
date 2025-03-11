4433c6d2cb8aed3e9ccfbb454c49f38ee3ee8e99
Fxuluxe <40676@prot.ac.th>
2025-03-11 11:48:38 +0700

    Update string_are_arrays.py

--- cell05/ex12/string_are_arrays.py
+++ cell05/ex12/string_are_arrays.py
@@ -1,1 +1,12 @@
-
+import sys
+sys.argv = ["scan_it.py", "apple"]
+if len(sys.argv) != 2:
+    print("none")
+      
+input_string = sys.argv[1]
+if 'z' not in input_string:
+    print("none")
+else:
+    for char in input_string:
+        if char == 'z':
+                print("z")
