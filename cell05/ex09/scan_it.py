a29813ca28e71dcb94de43c462dee715b5c56b27
Fxuluxe <40676@prot.ac.th>
2025-03-11 11:31:22 +0700

    Update scan_it.py

--- cell05/ex09/scan_it.py
+++ cell05/ex09/scan_it.py
@@ -1,19 +1,1 @@
-import sys
 
-def scan_it():
-    if len(sys.argv) != 3:
-        print("none")
-        return
-    
-    keyword = sys.argv[1]
-    string = sys.argv[2]
- 
-    count = string.count(keyword)
-    
-    if count == 0:
-        print("none")
-    else:
-        print(count)
-
-if __name__ == "__main__":
-    scan_it()
